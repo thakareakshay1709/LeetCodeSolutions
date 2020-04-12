6ca7ba4dcc945f804a96d1934a1afcf562820aa7
Akshay Thakare <thakare.akshay1709@gmail.com>
2020-04-12 20:39:32 +0100

    Submitting solution for Diameter of Binary Tree - Day 11/30

---
+++ LeetCodeChallange/src/leetcode/challange/TestAll.java
@@ -0,0 +1,16 @@
+package leetcode.challange;
+
+
+/**
+ * This class is created to test functionalites of codes
+ * @author akshaythakare
+ *
+ */
+public class TestAll {
+
+	public static void main(String[] args) 
+	{
+		
+	}
+
+}
