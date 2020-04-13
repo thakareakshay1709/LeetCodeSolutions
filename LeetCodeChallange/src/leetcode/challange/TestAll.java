7288ef3a71ffbe8498df5ee5e38062e0e969f9f2
Akshay Thakare <thakare.akshay1709@gmail.com>
2020-04-13 19:57:52 +0100

    Adding java solution of Last Stone Weight - Day 12/30

--- LeetCodeChallange/src/leetcode/challange/TestAll.java
+++ LeetCodeChallange/src/leetcode/challange/TestAll.java
@@ -10,7 +10,9 @@ public class TestAll {
 
 	public static void main(String[] args) 
 	{
-		
+		int[] arr = {2,7,4,1,8,1};
+		LastStoneWeight_Day12 test = new LastStoneWeight_Day12();
+		System.out.println(test.lastStoneWeight(arr));
 	}
 
 }
