24c588a07b76e56dec4b7db3be041a47a92fcc03
Akshay Thakare <thakare.akshay1709@gmail.com>
2020-04-20 18:50:26 +0100

    Submitting Java solution for Search In Rotated Array

---
+++ LeetCodeChallange/src/leetcode/challange/SearchInRotatedArray.java
@@ -0,0 +1,52 @@
+package leetcode.challange;
+
+public class SearchInRotatedArray {
+
+	public static void main(String[] args) 
+	{
+		int[] nums = {4,5,6,7,0,1,2};
+		int target = 0;
+		System.out.println(search(nums, target));
+
+	}
+	
+	public static int search(int[] nums, int target) 
+	{
+	    return binarySearch(nums, 0, nums.length-1, target);
+	}
+	 
+	public static int binarySearch(int[] nums, int left, int right, int target)
+	{
+	    if(left>right) 
+	        return -1;
+	 
+	    int mid = left + (right-left)/2;
+	 
+	    if(target == nums[mid])
+	        return mid;
+	 
+	    if(nums[left] <= nums[mid])
+	    {
+	        if(nums[left]<=target && target<nums[mid])
+	        {
+	          return binarySearch(nums,left, mid-1, target);
+	        }
+	        else
+	        {
+	          return  binarySearch(nums, mid+1, right, target);
+	        }
+	    }
+	    else 
+	    {
+	        if(nums[mid]<target&& target<=nums[right])
+	        {
+	          return  binarySearch(nums,mid+1, right, target);
+	        }
+	        else
+	        {
+	          return  binarySearch(nums, left, mid-1, target);
+	        }
+	    }
+	}
+
+}
