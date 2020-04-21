529976f4d2f89a3bdfb61b5b5717b67ff33356b2
Akshay Thakare <thakare.akshay1709@gmail.com>
2020-04-21 22:48:30 +0100

    Submitting solution Binary Search Traversal Preorder for day 20/30

---
+++ LeetCodeChallange/src/leetcode/challange/ConstructBSTPreorderTraversal.java
@@ -0,0 +1,35 @@
+package leetcode.challange;
+
+import java.util.Stack;
+
+public class ConstructBSTPreorderTraversal 
+{
+
+	public TreeNode bstFromPreorder (int[] preorder)
+	{
+		TreeNode root = new TreeNode(preorder[0]);
+		Stack<TreeNode> s = new Stack<>();
+		s.push(root);
+		
+		for(int i=1;i<preorder.length;i++)
+		{
+			TreeNode temp = null;
+			while(!s.isEmpty() && preorder[i]>s.peek().val)
+				temp = s.pop();
+			if(temp !=null)
+			{
+				temp.right = new TreeNode(preorder[i]);
+				s.push(temp.right);
+					
+			}
+			else
+			{
+				temp = s.peek();
+				temp.left = new TreeNode(preorder[i]);
+				s.push(temp.left);
+			}
+			
+		}
+		return root;
+	}
+}
