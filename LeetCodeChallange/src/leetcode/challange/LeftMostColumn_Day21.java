a860861be7db9f572663a6c2d558ed9a2ab33812
Akshay Thakare <thakare.akshay1709@gmail.com>
2020-04-22 22:34:17 +0100

    Adding java solution for left most column in binary matrix - Day 21/30

---
+++ LeetCodeChallange/src/leetcode/challange/LeftMostColumn_Day21.java
@@ -0,0 +1,37 @@
+package leetcode.challange;
+
+import java.util.List;
+
+public class LeftMostColumn_Day21 
+{
+	public int leftMostColumnWithOne(BinaryMatrix binaryMatrix)
+	{
+	    public int leftMostColumnWithOne(BinaryMatrix binaryMatrix) 
+	    {
+	        List<Integer> dim = binaryMatrix.dimensions();
+	        int rows = dim.get(0);
+	        int columns = dim.get(1);
+	        
+	        if(rows == 0 || columns == 0)
+	        {
+	            return -1;
+	        }
+	        
+	        int result = -1;
+	        int r = 0, c = columns -1;
+	        while(r < rows && c >=0)
+	        {
+	            if(binaryMatrix.get(r,c) ==1)
+	            {
+	                result = c;
+	                c--;
+	            }
+	            else
+	            {
+	                r++;
+	            }
+	        }
+	        return result;
+	    }
+	}
+}
