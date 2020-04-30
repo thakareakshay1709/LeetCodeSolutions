020290ba215bfebae9d12d5af8b6694ce35c3ca9
Akshay Thakare <thakare.akshay1709@gmail.com>
2020-04-30 13:08:34 +0100

    Adding solution for Binary Tree Maximum Path Sum - Day 29/30

---
+++ LeetCodeChallange/src/leetcode/challange/BinaryTreeMaximumPathSum_Day29.java
@@ -0,0 +1,59 @@
+package leetcode.challange;
+
+/**
+ * Given a non-empty binary tree, find the maximum path sum.
+
+For this problem, a path is defined as any sequence of nodes from some starting node to any node in the tree along the parent-child connections. The path must contain at least one node and does not need to go through the root.
+
+Example 1:
+
+Input: [1,2,3]
+
+       1
+      / \
+     2   3
+
+Output: 6
+Example 2:
+
+Input: [-10,9,20,null,null,15,7]
+
+   -10
+   / \
+  9  20
+    /  \
+   15   7
+
+Output: 42
+ * @author akshaythakare
+ *
+ */
+public class BinaryTreeMaximumPathSum_Day29 
+{
+
+	public int maxPathSum(TreeNode_Day29 root) 
+	{
+		int max[] = new int[1];
+		
+		max[0] = Integer.MIN_VALUE;
+		
+		calculateSum(root, max);
+		
+		return max[0];
+	}
+	 
+	public int calculateSum(TreeNode_Day29 root, int[] max) 
+	{
+		if (root == null)
+			return 0;
+	 
+		int left = calculateSum(root.left, max);
+		int right = calculateSum(root.right, max);
+	 
+		int current = Math.max(root.val, Math.max(root.val + left, root.val + right));
+	 
+		max[0] = Math.max(max[0], Math.max(current, left + root.val + right));
+	 
+		return current;
+	}
+}
---
+++ LeetCodeChallange/src/leetcode/challange/TreeNode_Day29.java
@@ -0,0 +1,19 @@
+package leetcode.challange;
+
+public class TreeNode_Day29 {
+
+	 //Definition for a binary tree node.
+	int val;
+	TreeNode_Day29 left;
+	TreeNode_Day29 right;
+	TreeNode_Day29() {}
+	TreeNode_Day29(int val) { this.val = val; }
+	TreeNode_Day29(int val, TreeNode_Day29 left, TreeNode_Day29 right) 
+	{
+		this.val = val;
+		this.left = left;
+		this.right = right;
+	}
+	  
+	 
+}
