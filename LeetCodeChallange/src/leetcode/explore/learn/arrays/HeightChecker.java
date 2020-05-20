5d15acb42669cbd5a696d44318e168fa0d8a36a1
Akshay Thakare <thakare.akshay1709@gmail.com>
2020-05-20 21:51:08 +0100

    Adding java solution for Height Checker java - Leetcode Arrays

---
+++ LeetCodeChallange/src/leetcode/explore/learn/arrays/HeightChecker.java
@@ -0,0 +1,81 @@
+package leetcode.explore.learn.arrays;
+
+import java.util.Arrays;
+
+public class HeightChecker {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		int[] height = {1,1,4,2,1,3};
+		System.out.println(heightChecker(height));
+	}
+	
+	public static int heightChecker(int[] heights) 
+	{
+		if(heights.length == 1)
+		{
+			return 0;
+		}
+		int count = 0;
+		int[] newSortedHeights = new int[heights.length];
+		for (int i = 0; i < heights.length; i++) 
+		{
+			newSortedHeights[i] = heights[i];
+		}
+		Arrays.sort(newSortedHeights);
+		
+		
+		
+		for (int i = 0,j=0; i < newSortedHeights.length && j < heights.length; i++,j++) 
+		{
+			if(newSortedHeights[i] != heights[j])
+			{
+				count++;
+			}
+			//System.out.print(newSortedHeights[i]+" ");
+		}
+        return count;
+    }
+
+}
+
+
+
+/*
+ * Students are asked to stand in non-decreasing order of heights for an annual photo.
+
+Return the minimum number of students that must move in order for all students to be standing in non-decreasing order of height.
+
+Notice that when a group of students is selected they can reorder in any possible way between themselves and the non selected students remain on their seats.
+
+ 
+
+Example 1:
+
+Input: heights = [1,1,4,2,1,3]
+Output: 3
+Explanation: 
+Current array : [1,1,4,2,1,3]
+Target array  : [1,1,1,2,3,4]
+On index 2 (0-based) we have 4 vs 1 so we have to move this student.
+On index 4 (0-based) we have 1 vs 3 so we have to move this student.
+On index 5 (0-based) we have 3 vs 4 so we have to move this student.
+Example 2:
+
+Input: heights = [5,1,2,3,4]
+Output: 5
+Example 3:
+
+Input: heights = [1,2,3,4,5]
+Output: 0
+ 
+
+Constraints:
+
+1 <= heights.length <= 100
+1 <= heights[i] <= 100
+   Hide Hint #1  
+Build the correct order of heights by sorting another array, then compare the two arrays.
+ * *
+ */
