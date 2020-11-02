8440c9e12280aa5276efaf888566c16c951585fa
Akshay Thakare <thakare.akshay1709@gmail.com>
2020-11-02 21:29:30 +0000

    Reverse Words in a String III

---
+++ LeetCodeChallange/src/leetcode/explore/learn/arrayandstring/ReverseStringIII.java
@@ -0,0 +1,25 @@
+package leetcode.explore.learn.arrayandstring;
+
+public class ReverseStringIII {
+	  public String reverseWords(String s) {
+	    int idx = 0;
+	    int n = s.length();
+	    StringBuilder sb = new StringBuilder();
+	    int start = 0;
+	    while (idx < n) {
+	      while (idx < n && s.charAt(idx) != ' ') {
+	        idx++;
+	      }
+	      int curr = idx - 1;
+	      while (curr >= start) {
+	        sb.append(s.charAt(curr--));
+	      }
+	      if (idx != n) {
+	        sb.append(" ");
+	      }
+	      idx++;
+	      start = idx;
+	    }
+	    return sb.toString();
+	  }
+}
